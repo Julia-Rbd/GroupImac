903b9b08038a0422938f8bc35980adb109ccee0d
ClemThierry <49915513+ClemThierry@users.noreply.github.com>
2021-04-23 18:35:53 +0200

    Première version avec sessions et accès à un pseudo espace membre
    C'est pas ouf pour l'instant mais ça prend forme

---
+++ Session+Profil/connexion.php
@@ -0,0 +1,19 @@
+<?php
+require_once ("model.php");
+if (isset($_POST['connexion']) && $_POST['connexion'] == 'Connexion') {
+    if ((isset($_POST['login']) && !empty($_POST['login'])) && (isset($_POST['pwd']) && !empty($_POST['pwd']))) {
+            if (getMember($_POST['login'],$_POST['pwd'])!=false) {
+                session_start();
+                $_SESSION['infoMembre'] = getMember($_POST['login'],$_POST['pwd']);
+                header('Location: espace-membre.php');
+            }else {
+            $erreur = 'Login ou mot de passe non reconnu !';
+            echo $erreur; 
+            echo"<br/><a href=\"accueil.php\">Accueil</a>";
+            }
+        }else {
+$erreur = 'Errreur de saisie !<br/>Au moins un des champs est vide !'; 
+echo $erreur; 
+echo"<br/><a href=\"accueil.php\">Accueil</a>";
+}
+}
---
+++ Session+Profil/verification.php
@@ -0,0 +1,32 @@
+<?php
+require_once ("model.php");
+if (isset($_POST['inscription']) && $_POST['inscription'] == 'Inscription') {
+   if ((isset($_POST['prenom']) && !empty($_POST['prenom'])) && (isset($_POST['nom']) && !empty($_POST['nom'])) && (isset($_POST['mdp1']) && !empty($_POST['mdp1'])) && (isset($_POST['mdp2']) && !empty($_POST['mdp2'])) && (isset($_POST['promo']) && !empty($_POST['promo']))) 
+   {
+      if ($_POST['mdp1'] != $_POST['mdp2']) 
+      {
+         $erreur = 'Les 2 password sont differents.';
+         echo $erreur; 
+         echo"<br/><a href=\"accueil.php\">Accueil</a>";
+         exit();
+      }else {	
+      //si les deux mots de passe sont identiques, on se connecte à la bdd
+         if (addMember($_POST['nom'],$_POST['prenom'],$_POST['mdp1'],$_POST['promo'])) {
+            session_start();
+            $_SESSION['infoMembre'] = getMember($_POST['prenom'],$_POST['mdp1']);
+            header('Location: espace-membre.php');
+         }else {
+            $erreur = 'Echec de l\'inscription !<br/>Un membre possede deja ce login !';
+            echo $erreur; 
+            echo"<br/><a href=\"accueil.php\">Accueil</a>";
+         }
+      }
+   //Si au moins un des champs est vide
+   }else {
+   $erreur = 'Echec de l\'inscription !<br/>Au moins un des champs est vide !';
+   echo $erreur; 
+   echo"<br/><a href=\"accueil.php\">Accueil</a>";
+   }
+}
+?>
+
